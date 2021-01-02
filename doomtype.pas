58a8051e6842ce5d7df36e61cac68cd48f0bfcdb
jval1972 <jvalavanis@gmail.com>
2021-01-02 15:26:19 +0200

    Update doomtype.pas

--- doomtype.pas
+++ doomtype.pas
@@ -28,53 +28,26 @@ unit doomtype;
 
 interface
 
-{
-    doomtype.h
-}
-
-  { Emacs style mode select   -*- C++ -*-  }
-  {----------------------------------------------------------------------------- }
-  { }
-  { $Id:$ }
-  { }
-  { Copyright (C) 1993-1996 by id Software, Inc. }
-  { }
-  { This source is available for distribution and/or modification }
-  { only under the terms of the DOOM Source Code License as }
-  { published by id Software. All rights reserved. }
-  { }
-  { The source is distributed in the hope that it will be useful, }
-  { but WITHOUT ANY WARRANTY; without even the implied warranty of }
-  { FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License }
-  { for more details. }
-  { }
-  { DESCRIPTION: }
-  {	Simple basic typedefs, isolated here to make it easier }
-  {	 separating modules. }
-  {     }
-  {----------------------------------------------------------------------------- }
-
 type
   bool = boolean;
 
+function MAXCHAR: char;
 
-function MAXCHAR : char;
-
-function MAXSHORT : smallint;
+function MAXSHORT: smallint;
 
 { Max pos 32-bit int. }
-function MAXINT : longint;
+function MAXINT: longint;
 
-function MAXLONG : longint;
+function MAXLONG: longint;
 
-function MINCHAR : char;
+function MINCHAR: char;
 
-function MINSHORT : smallint;
+function MINSHORT: smallint;
 
 { Max negative 32-bit integer. }
-function MININT : longint;
+function MININT: longint;
 
-function MINLONG : longint;
+function MINLONG: longint;
 
 implementation
 
@@ -83,37 +56,37 @@ begin
   MAXCHAR := char($7f);
 end;
 
-function MAXSHORT : smallint;
+function MAXSHORT: smallint;
 begin
   MAXSHORT := smallint($7fff);
 end;
 
-function MAXINT : longint;
+function MAXINT: longint;
 begin
   MAXINT := longint($7fffffff);
 end;
 
-function MAXLONG : longint;
+function MAXLONG: longint;
 begin
   MAXLONG := longint($7fffffff);
 end;
 
-function MINCHAR : char;
+function MINCHAR: char;
 begin
   MINCHAR := char($80);
 end;
 
-function MINSHORT : smallint;
+function MINSHORT: smallint;
 begin
   MINSHORT := smallint($8000);
 end;
 
-function MININT : longint;
+function MININT: longint;
 begin
   MININT := longint($80000000);
 end;
 
-function MINLONG : longint;
+function MINLONG: longint;
 begin
   MINLONG := longint($80000000);
 end;
