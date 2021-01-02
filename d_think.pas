d3713b70c329a2d979cc1537407eba974cbeb535
jval1972 <jvalavanis@gmail.com>
2021-01-02 15:22:07 +0200

    Update d_think.pas

--- d_think.pas
+++ d_think.pas
@@ -28,33 +28,6 @@ unit d_think;
 
 interface
 
-{
-    d_think.h
-}
-
-  { Emacs style mode select   -*- C++ -*-  }
-  {----------------------------------------------------------------------------- }
-  { }
-  { $Id:$ }
-  { }
-  { Copyright (C) 1993-1996 by id Software, Inc. }
-  { }
-  { This source is available for distribution and/or modification }
-  { only under the terms of the DOOM Source Code License as }
-  { published by id Software. All rights reserved. }
-  { }
-  { The source is distributed in the hope that it will be useful, }
-  { but WITHOUT ANY WARRANTY; without even the implied warranty of }
-  { FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License }
-  { for more details. }
-  { }
-  { DESCRIPTION: }
-  {  MapObj data. Map Objects or mobjs are actors, entities, }
-  {  thinker, take-your-pick... anything that moves, acts, or }
-  {  suffers state changes of more or less violent nature. }
-  { }
-  {----------------------------------------------------------------------------- }
-
 type
   actionf_v = procedure;
   actionf_p1 = procedure(p1: pointer);
