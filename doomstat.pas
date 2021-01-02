c8fdadc90689e287d65c9c4a3c939364bdf3675f
jval1972 <jvalavanis@gmail.com>
2021-01-02 15:25:29 +0200

    Update doomstat.pas

--- doomstat.pas
+++ doomstat.pas
@@ -28,53 +28,21 @@ unit doomstat;
 
 interface
 
-uses doomdef;
-
-{
-    doomstat.c
-}
-
-  { Emacs style mode select   -*- C++ -*-  }
-  {----------------------------------------------------------------------------- }
-  { }
-  { $Id:$ }
-  { }
-  { Copyright (C) 1993-1996 by id Software, Inc. }
-  { }
-  { This source is available for distribution and/or modification }
-  { only under the terms of the DOOM Source Code License as }
-  { published by id Software. All rights reserved. }
-  { }
-  { The source is distributed in the hope that it will be useful, }
-  { but WITHOUT ANY WARRANTY; without even the implied warranty of }
-  { FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License }
-  { for more details. }
-  { }
-  { $Log:$ }
-  { }
-  { DESCRIPTION: }
-  {	Put all global tate variables here. }
-  { }
-  {----------------------------------------------------------------------------- }
+uses
+  doomdef;
 
 var
 // Game Mode - identify IWAD as shareware, retail etc.
-  gamemode: GameMode_t;
-  gamemission: GameMission_t;
+  gamemode: GameMode_t = indetermined;
+  gamemission: GameMission_t = doom;
 
 // Language.
-  language: Language_t;
+  language: Language_t = english;
 
 // Set if homebrew PWAD stuff has been added.
-  modifiedgame : boolean;
+  modifiedgame: boolean = false;
 
 implementation
-
-initialization
-  gamemode := indetermined;
-  gamemission := doom;
-  language := english;
-  modifiedgame := false;
 
 end.
 
