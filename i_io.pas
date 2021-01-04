c728b488bb1b6ef3c68c5f64a0b288014dbcaf7d
jval1972 <jvalavanis@gmail.com>
2021-01-04 23:31:34 +0200

    Update i_io.pas

--- i_io.pas
+++ i_io.pas
@@ -1,8 +1,34 @@
+//------------------------------------------------------------------------------
+//
+//  DoomXS - A basic Windows source port of Doom
+//  based on original Linux Doom as published by "id Software"
+//  Copyright (C) 1993-1996 by id Software, Inc.
+//  Copyright (C) 2021 by Jim Valavanis
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, inc., 59 Temple Place - Suite 330, Boston, MA
+//  02111-1307, USA.
+//
+//------------------------------------------------------------------------------
+//  Site: https://sourceforge.net/projects/doomxs/
+//------------------------------------------------------------------------------
+
 unit i_io;
 
 interface
 
-uses 
+uses
   d_delphi;
 
 var
@@ -21,7 +47,8 @@ procedure I_IOprintf(const s: string);
 
 implementation
 
-uses Windows,
+uses
+  Windows,
   g_game,
   i_main,
   m_argv;
