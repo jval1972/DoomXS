9f211c0ec75562d0ced9be750d41482ef90d708b
jval1972 <jvalavanis@gmail.com>
2021-01-11 04:28:35 +0200

    Update sounds.pas

--- sounds.pas
+++ sounds.pas
@@ -28,32 +28,6 @@ unit sounds;
 
 interface
 
-{
-    sounds.h, sounds.c
-}
-
-// Emacs style mode select   -*- C++ -*- 
-//-----------------------------------------------------------------------------
-//
-// $Id:$
-//
-// Copyright (C) 1993-1996 by id Software, Inc.
-//
-// This source is available for distribution and/or modification
-// only under the terms of the DOOM Source Code License as
-// published by id Software. All rights reserved.
-//
-// The source is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
-// for more details.
-//
-// DESCRIPTION:
-//	Created by the sound utility written by Dave Taylor.
-//	Kept as a sample, DOOM2  sounds. Frozen.
-//
-//-----------------------------------------------------------------------------
-
 //
 // SoundFX struct.
 //
