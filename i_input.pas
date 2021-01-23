edace79f3ca729f489d513f76d7d193bfea714d9
jval1972 <jvalavanis@gmail.com>
2021-01-23 12:37:14 +0200

    Removed non-working DirectInput

--- i_input.pas
+++ i_input.pas
@@ -32,18 +32,14 @@ procedure I_InitInput;
 
 procedure I_ProcessInput;
 
-procedure I_ShutdownInput;
+procedure I_ShutDownInput;
 
 procedure I_SynchronizeInput(active: boolean);
-
-var
-  usedirectinput: boolean = False;
 
 implementation
 
 uses
   Windows,
-  directx,
   d_delphi,
   doomdef,
   d_event,
@@ -106,9 +102,6 @@ begin
 end;
 
 var
-  g_pDI: IDirectInputA = nil;
-  g_pdidKeyboard: IDirectInputDevice = nil;
-  dikb: TDIKeyboardState; // DirectInput keyboard state buffer
   curkeys: PKeyboardState;
   oldkeys: PKeyboardState;
 
@@ -121,77 +114,17 @@ var
 //           IDirectInputDevice::SetCooperativeLevel
 //-----------------------------------------------------------------------------
 procedure I_InitInput;
-var
-  hres: HRESULT;
-
-  procedure I_ErrorInitInput(const msg: string);
-  begin
-    I_Error('I_InitInput(): %s failed, result = %d', [msg, hres]);
-  end;
-
 begin
-  if usedirectinput then
-  begin
-    // Register with the DirectInput subsystem and get a pointer
-    // to a IDirectInput interface we can use
-    hres := DirectInputCreate(hInstance, DIRECTINPUT_VERSION, g_pDI, nil);
-    if hres <> DD_OK then
-      I_ErrorInitInput('DirectInputCreate');
-
-    // Obtain an interface to the keybord device
-    hres := g_pDI.CreateDevice(GUID_SysKeyboard, g_pdidKeyboard, nil);
-    if hres <> DD_OK then
-      I_ErrorInitInput('CreateDevice');
-
-    // Set the data format to "keyboard format". A data format specifies which
-    // controls on a device we are interested in, and how they should be
-    // reported. This tells DirectInput that we will be passing an array of 256
-    // bytes to IDirectInputDevice::GetDeviceState().
-    hres := g_pdidKeyboard.SetDataFormat(c_dfDIKeyboard);
-    if hres <> DD_OK then
-      I_ErrorInitInput('SetDataFormat');
-
-    // Set the cooperative level to let DirectInput know how this device
-    // should interact with the system and with other DirectInput applications.
-    // Use DISCL_NONEXCLUSIVE to retrieve device data when acquired, not
-    // interfering with any other applications which are reading mouse data.
-    // Use DISCL_FOREGROUND so that if the user switches away from our app,
-    // automatically release the device back to the system.
-    hres := g_pdidKeyboard.SetCooperativeLevel(hMainWnd, DISCL_NONEXCLUSIVE or
-      DISCL_FOREGROUND);
-    if hres <> DD_OK then
-      I_ErrorInitInput('SetCooperativeLevel');
-
-  end;
-
   curkeys := malloc(SizeOf(TKeyboardState));
   oldkeys := malloc(SizeOf(TKeyboardState));
-
 end;
 
 //-----------------------------------------------------------------------------
-// Name: I_ShutdownInput
+// Name: I_ShutDownInput
 // Desc: Terminate our usage of DirectInput
 //-----------------------------------------------------------------------------
-procedure I_ShutdownInput;
+procedure I_ShutDownInput;
 begin
-  if usedirectinput then
-  begin
-    if g_pDI <> nil then
-    begin
-      // Destroy the keyboard object
-      if g_pdidKeyboard <> nil then
-      begin
-        // Unacquire the device (just in case) before exitting.
-        g_pdidKeyboard.Unacquire;
-        g_pdidKeyboard._Release;
-      end;
-
-      // Destroy the DInput object
-      g_pDI._Release;
-    end;
-  end;
-
   FreeMem(curkeys);
   FreeMem(oldkeys);
 end;
@@ -201,151 +134,21 @@ end;
 // Desc: The game plays here. Read keyboard data and displaying it.
 //-----------------------------------------------------------------------------
 procedure I_ProcessInput;
-
-  function DIKEYtoVK(Key: byte): integer;
-  begin
-    Result := 0;
-    case Key of
-      DIK_ESCAPE: Result := VK_ESCAPE;
-      DIK_1: Result := Ord('1');
-      DIK_2: Result := Ord('2');
-      DIK_3: Result := Ord('3');
-      DIK_4: Result := Ord('4');
-      DIK_5: Result := Ord('5');
-      DIK_6: Result := Ord('6');
-      DIK_7: Result := Ord('7');
-      DIK_8: Result := Ord('8');
-      DIK_9: Result := Ord('9');
-      DIK_0: Result := Ord('0');
-      DIK_EQUALS: Result := Ord('=');
-      DIK_BACK: Result := VK_BACK;
-      DIK_TAB: Result := VK_TAB;
-      DIK_Q: Result := Ord('Q');
-      DIK_W: Result := Ord('W');
-      DIK_E: Result := Ord('E');
-      DIK_R: Result := Ord('R');
-      DIK_T: Result := Ord('T');
-      DIK_Y: Result := Ord('Y');
-      DIK_U: Result := Ord('U');
-      DIK_I: Result := Ord('I');
-      DIK_O: Result := Ord('O');
-      DIK_P: Result := Ord('P');
-      DIK_LBRACKET: Result := Ord('[');
-      DIK_RBRACKET: Result := Ord(']');
-      DIK_RETURN: Result := VK_RETURN;
-      DIK_LCONTROL: Result := VK_CONTROL;
-      DIK_A: Result := Ord('A');
-      DIK_S: Result := Ord('S');
-      DIK_D: Result := Ord('D');
-      DIK_F: Result := Ord('F');
-      DIK_G: Result := Ord('G');
-      DIK_H: Result := Ord('H');
-      DIK_J: Result := Ord('J');
-      DIK_K: Result := Ord('K');
-      DIK_L: Result := Ord('L');
-      DIK_SEMICOLON: Result := Ord(';');
-      DIK_APOSTROPHE: Result := Ord('''');
-      DIK_LSHIFT: Result := VK_SHIFT;
-      DIK_BACKSLASH: Result := Ord('\');
-      DIK_Z: Result := Ord('Z');
-      DIK_X: Result := Ord('X');
-      DIK_C: Result := Ord('C');
-      DIK_V: Result := Ord('V');
-      DIK_B: Result := Ord('B');
-      DIK_N: Result := Ord('N');
-      DIK_M: Result := Ord('M');
-      DIK_COMMA: Result := Ord(',');
-      DIK_PERIOD: Result := Ord('.');
-      DIK_SLASH: Result := Ord('/');
-      DIK_RSHIFT: Result := VK_SHIFT;
-      DIK_MULTIPLY: Result := Ord('*');
-      DIK_LMENU: Result := VK_MENU;
-      DIK_SPACE: Result := VK_SPACE;
-      DIK_CAPITAL: Result := VK_CAPITAL;
-      DIK_F1: Result := VK_F1;
-      DIK_F2: Result := VK_F2;
-      DIK_F3: Result := VK_F3;
-      DIK_F4: Result := VK_F4;
-      DIK_F5: Result := VK_F5;
-      DIK_F6: Result := VK_F6;
-      DIK_F7: Result := VK_F7;
-      DIK_F8: Result := VK_F8;
-      DIK_F9: Result := VK_F9;
-      DIK_F10: Result := VK_F10;
-      DIK_NUMLOCK: Result := VK_NUMLOCK;
-      DIK_SCROLL: Result := VK_SCROLL;
-      DIK_NUMPAD7: Result := VK_NUMPAD7;
-      DIK_NUMPAD8: Result := VK_NUMPAD8;
-      DIK_NUMPAD9: Result := VK_NUMPAD9;
-      DIK_SUBTRACT: Result := VK_SUBTRACT;
-      DIK_NUMPAD4: Result := VK_NUMPAD4;
-      DIK_NUMPAD5: Result := VK_NUMPAD5;
-      DIK_NUMPAD6: Result := VK_NUMPAD6;
-      DIK_ADD: Result := VK_ADD;
-      DIK_NUMPAD1: Result := VK_NUMPAD1;
-      DIK_NUMPAD2: Result := VK_NUMPAD2;
-      DIK_NUMPAD3: Result := VK_NUMPAD3;
-      DIK_NUMPAD0: Result := VK_NUMPAD0;
-      DIK_DECIMAL: Result := VK_DECIMAL;
-      DIK_F11: Result := VK_F11;
-      DIK_F12: Result := VK_F12;
-      DIK_NUMPADENTER: Result := VK_RETURN;
-      DIK_RCONTROL: Result := VK_CONTROL;
-      DIK_DIVIDE: Result := VK_DIVIDE;
-      DIK_RMENU: Result := VK_MENU;
-      DIK_HOME: Result := VK_HOME;
-      DIK_UP: Result := VK_UP;
-      DIK_PRIOR: Result := VK_PRIOR;
-      DIK_LEFT: Result := VK_LEFT;
-      DIK_RIGHT: Result := VK_RIGHT;
-      DIK_END: Result := VK_END;
-      DIK_DOWN: Result := VK_DOWN;
-      DIK_NEXT: Result := VK_NEXT;
-      DIK_INSERT: Result := VK_INSERT;
-      DIK_DELETE: Result := VK_DELETE;
-      DIK_LWIN: Result := VK_LWIN;
-      DIK_RWIN: Result := VK_RWIN;
-      DIK_APPS: Result := VK_APPS;
-    end;
-  end;
-
 var
-  hres: HRESULT;
   i: integer;
   ev: event_t;
   key: integer;
   p: PKeyboardState;
 begin
   if I_GameFinished then
-    exit;
+    Åxit;
 
-  // VJ -> DirectInput does not work
-  if usedirectinput and (g_pdidKeyboard <> nil) then
-  begin
-    hres := g_pdidKeyboard.GetDeviceState(SizeOf(dikb), dikb);
-    if hres = DIERR_INPUTLOST then
-    begin
-      // DirectInput is telling us that the input stream has been
-      // interrupted. Re-acquire and try again.
-      hres := g_pdidKeyboard.Acquire;
-      if hres = DD_OK then
-        I_ProcessInput;
-      exit;
-    end;
-
-    //  The DirectInput key code is converted into the Windows virtual key code.
-    for i := Low(dikb) to High(dikb) do
-      if dikb[i] and $80 <> 0 then
-        curkeys[byte(DIKEYtoVK(i))] := $80;
-  end
-  else
-    GetKeyboardState(curkeys^);
+  GetKeyboardState(curkeys^);
 
   ZeroMemory(ev, SizeOf(ev));
 
   for i := 0 to SizeOf(curkeys^) - 1 do
   begin
-
     if (oldkeys[i] and $80) <> (curkeys[i] and $80) then
     begin
       key := TranslateKey(i);
@@ -370,7 +173,6 @@ begin
         D_PostEvent(@ev);
       end;
     end;
-
   end;
 
   p := oldkeys;
@@ -380,13 +182,6 @@ end;
 
 procedure I_SynchronizeInput(active: boolean);
 begin
-  if usedirectinput and (g_pdidKeyboard <> nil) then
-  begin
-    if active then
-      g_pdidKeyboard.Acquire
-    else
-      g_pdidKeyboard.Unacquire;
-  end;
 end;
 
 end.
