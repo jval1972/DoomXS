3d687f39e8dae4e5b2cee95608414d18017da209
jval1972 <jvalavanis@gmail.com>
2021-05-07 13:01:36 +0300

    weaponinfo array made const

--- d_items.pas
+++ d_items.pas
@@ -29,112 +29,63 @@ unit d_items;
 interface
 
 uses
-  doomdef;
+  doomdef,
+  info_h;
 
 type
   { Weapon info: sprite frames, ammunition use. }
   weaponinfo_t = record
     ammo: ammotype_t;
-    upstate: longint;
-    downstate: longint;
-    readystate: longint;
-    atkstate: longint;
-    flashstate: longint;
+    upstate: integer;
+    downstate: integer;
+    readystate: integer;
+    atkstate: integer;
+    flashstate: integer;
   end;
+  Pweaponinfo_t = ^weaponinfo_t;
 
-var
-  weaponinfo: array[0..Ord(NUMWEAPONS)-1] of weaponinfo_t;
-
-implementation
-
-uses
-  info_h;
-
-initialization
-//
 // PSPRITE ACTIONS for waepons.
 // This struct controls the weapon animations.
-//
+
 // Each entry is:
 //   ammo/amunition type
-//  upstate
-//  downstate
-// readystate
-// atkstate, i.e. attack/fire/hit frame
-// flashstate, muzzle flash
-//
-
+//   upstate
+//   downstate
+//   readystate
+//   atkstate, i.e. attack/fire/hit frame
+//   flashstate, muzzle flash
+var
+  weaponinfo: array[0..Ord(NUMWEAPONS) - 1] of weaponinfo_t = (
   // fist
-  weaponinfo[0].ammo := am_noammo;
-  weaponinfo[0].upstate := Ord(S_PUNCHUP);
-  weaponinfo[0].downstate := Ord(S_PUNCHDOWN);
-  weaponinfo[0].readystate := Ord(S_PUNCH);
-  weaponinfo[0].atkstate := Ord(S_PUNCH1);
-  weaponinfo[0].flashstate := Ord(S_NULL);
-
+    (ammo: am_noammo;            upstate: Ord(S_PUNCHUP);   downstate: Ord(S_PUNCHDOWN);
+     readystate: Ord(S_PUNCH);   atkstate: Ord(S_PUNCH1);   flashstate: Ord(S_NULL)),
   // pistol
-  weaponinfo[1].ammo := am_clip;
-  weaponinfo[1].upstate := Ord(S_PISTOLUP);
-  weaponinfo[1].downstate := Ord(S_PISTOLDOWN);
-  weaponinfo[1].readystate := Ord(S_PISTOL);
-  weaponinfo[1].atkstate := Ord(S_PISTOL1);
-  weaponinfo[1].flashstate := Ord(S_PISTOLFLASH);
-
+    (ammo: am_clip;              upstate: Ord(S_PISTOLUP);  downstate: Ord(S_PISTOLDOWN);
+     readystate: Ord(S_PISTOL);  atkstate: Ord(S_PISTOL1);  flashstate: Ord(S_PISTOLFLASH)),
   // shotgun
-  weaponinfo[2].ammo := am_shell;
-  weaponinfo[2].upstate := Ord(S_SGUNUP);
-  weaponinfo[2].downstate := Ord(S_SGUNDOWN);
-  weaponinfo[2].readystate := Ord(S_SGUN);
-  weaponinfo[2].atkstate := Ord(S_SGUN1);
-  weaponinfo[2].flashstate := Ord(S_SGUNFLASH1);
-
+    (ammo: am_shell;             upstate: Ord(S_SGUNUP);    downstate: Ord(S_SGUNDOWN);
+     readystate: Ord(S_SGUN);    atkstate: Ord(S_SGUN1);    flashstate: Ord(S_SGUNFLASH1)),
   // chaingun
-  weaponinfo[3].ammo := am_clip;
-  weaponinfo[3].upstate := Ord(S_CHAINUP);
-  weaponinfo[3].downstate := Ord(S_CHAINDOWN);
-  weaponinfo[3].readystate := Ord(S_CHAIN);
-  weaponinfo[3].atkstate := Ord(S_CHAIN1);
-  weaponinfo[3].flashstate := Ord(S_CHAINFLASH1);
-
+    (ammo: am_clip;              upstate: Ord(S_CHAINUP);   downstate: Ord(S_CHAINDOWN);
+     readystate: Ord(S_CHAIN);   atkstate: Ord(S_CHAIN1);   flashstate: Ord(S_CHAINFLASH1)),
   // missile launcher
-  weaponinfo[4].ammo := am_misl;
-  weaponinfo[4].upstate := Ord(S_MISSILEUP);
-  weaponinfo[4].downstate := Ord(S_MISSILEDOWN);
-  weaponinfo[4].readystate := Ord(S_MISSILE);
-  weaponinfo[4].atkstate := Ord(S_MISSILE1);
-  weaponinfo[4].flashstate := Ord(S_MISSILEFLASH1);
-
+    (ammo: am_misl;              upstate: Ord(S_MISSILEUP); downstate: Ord(S_MISSILEDOWN);
+     readystate: Ord(S_MISSILE); atkstate: Ord(S_MISSILE1); flashstate: Ord(S_MISSILEFLASH1)),
   // plasma rifle
-  weaponinfo[5].ammo := am_cell;
-  weaponinfo[5].upstate := Ord(S_PLASMAUP);
-  weaponinfo[5].downstate := Ord(S_PLASMADOWN);
-  weaponinfo[5].readystate := Ord(S_PLASMA);
-  weaponinfo[5].atkstate := Ord(S_PLASMA1);
-  weaponinfo[5].flashstate := Ord(S_PLASMAFLASH1);
-
+     (ammo: am_cell;             upstate: Ord(S_PLASMAUP);  downstate: Ord(S_PLASMADOWN);
+      readystate: Ord(S_PLASMA); atkstate: Ord(S_PLASMA1);  flashstate: Ord(S_PLASMAFLASH1)),
   // bfg 9000
-  weaponinfo[6].ammo := am_cell;
-  weaponinfo[6].upstate := Ord(S_BFGUP);
-  weaponinfo[6].downstate := Ord(S_BFGDOWN);
-  weaponinfo[6].readystate := Ord(S_BFG);
-  weaponinfo[6].atkstate := Ord(S_BFG1);
-  weaponinfo[6].flashstate := Ord(S_BFGFLASH1);
-
+     (ammo: am_cell;             upstate: Ord(S_BFGUP);     downstate: Ord(S_BFGDOWN);
+      readystate: Ord(S_BFG);    atkstate: Ord(S_BFG1);     flashstate: Ord(S_BFGFLASH1)),
   // chainsaw
-  weaponinfo[7].ammo := am_noammo;
-  weaponinfo[7].upstate := Ord(S_SAWUP);
-  weaponinfo[7].downstate := Ord(S_SAWDOWN);
-  weaponinfo[7].readystate := Ord(S_SAW);
-  weaponinfo[7].atkstate := Ord(S_SAW1);
-  weaponinfo[7].flashstate := Ord(S_NULL);
-
+     (ammo: am_noammo;           upstate: Ord(S_SAWUP);     downstate: Ord(S_SAWDOWN);
+      readystate: Ord(S_SAW);    atkstate: Ord(S_SAW1);     flashstate: Ord(S_NULL)),
   // super shotgun
-  weaponinfo[8].ammo := am_shell;
-  weaponinfo[8].upstate := Ord(S_DSGUNUP);
-  weaponinfo[8].downstate := Ord(S_DSGUNDOWN);
-  weaponinfo[8].readystate := Ord(S_DSGUN);
-  weaponinfo[8].atkstate := Ord(S_DSGUN1);
-  weaponinfo[8].flashstate := Ord(S_DSGUNFLASH1);
+     (ammo: am_shell;            upstate: Ord(S_DSGUNUP);   downstate: Ord(S_DSGUNDOWN);
+      readystate: Ord(S_DSGUN);  atkstate: Ord(S_DSGUN1);   flashstate: Ord(S_DSGUNFLASH1))
+  );
+
+implementation
 
 end.
 
