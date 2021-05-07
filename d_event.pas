f7bc0b2577542e75d81cc507039bea6d2783bf2a
jval1972 <jvalavanis@gmail.com>
2021-05-07 15:54:52 +0300

    Update d_event.pas

--- d_event.pas
+++ d_event.pas
@@ -1,20 +1,20 @@
 //------------------------------------------------------------------------------
-
+//
 //  DoomXS - A basic Windows source port of Doom
 //  based on original Linux Doom as published by "id Software"
 //  Copyright (C) 1993-1996 by id Software, Inc.
 //  Copyright (C) 2021 by Jim Valavanis
-
+//
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
 //  as published by the Free Software Foundation; either version 2
 //  of the License, or (at your option) any later version.
-
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program; if not, write to the Free Software
 //  Foundation, inc., 59 Temple Place - Suite 330, Boston, MA
@@ -35,7 +35,6 @@ type
   { keys / mouse/joystick buttons }
   { mouse/joystick x move }
   { mouse/joystick y move }
-
   event_t = record
     typ: evtype_t;
     data1: integer;
@@ -82,7 +81,6 @@ const
   //  occupy the second byte of buttons.
   BTS_SAVEMASK = (4 + 8 + 16);
   BTS_SAVESHIFT = 2;
-
 
 { GLOBAL VARIABLES }
 const
