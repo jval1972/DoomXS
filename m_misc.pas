7f696bc137c6458bf6c58a64f44123cd600352b1
jval1972 <jvalavanis@gmail.com>
2022-04-03 18:10:45 +0300

    Default sound channels raised to 32

--- m_misc.pas
+++ m_misc.pas
@@ -17,7 +17,7 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program; if not, write to the Free Software
-//  Foundation, inc., 59 Temple Place - Suite 330, Boston, MA
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 //  02111-1307, USA.
 //
 //------------------------------------------------------------------------------
@@ -231,7 +231,7 @@ const
 
     (name: 'snd_channels';
      location: @numChannels;
-     defaultivalue: 8;
+     defaultivalue: 32;
      _type: tInteger),
 
     (name: 'usegamma';
