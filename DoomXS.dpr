a5726ad035777fc78692d276256d551fd87c4842
jval1972 <jvalavanis@gmail.com>
2022-04-12 20:30:38 +0300

    Update year in copyright notice

--- DoomXS.dpr
+++ DoomXS.dpr
@@ -3,7 +3,7 @@
 //  DoomXS - A basic Windows source port of Doom
 //  based on original Linux Doom as published by "id Software"
 //  Copyright (C) 1993-1996 by id Software, Inc.
-//  Copyright (C) 2021 by Jim Valavanis
+//  Copyright (C) 2021-2022 by Jim Valavanis
 
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
